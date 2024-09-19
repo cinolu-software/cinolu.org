32ed4a37a2f209f03956044e565643107c7d2cfa
Wilfried Musanzi <musanziw.m@gmail.com>
2024-09-19 12:12:45 +0200

    style: display all member

--- src/app/modules/landing/components/team/team.component.ts
+++ src/app/modules/landing/components/team/team.component.ts
@@ -2,41 +2,13 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { team } from '../../data/team';
 import { ObserveVisibilityDirective } from 'app/core/directives/observer.directive';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'landing-team',
   standalone: true,
-  imports: [NgOptimizedImage, ObserveVisibilityDirective, MatIconModule, CommonModule],
+  imports: [NgOptimizedImage, ObserveVisibilityDirective, CommonModule],
   templateUrl: './team.component.html'
 })
 export class TeamComponent {
   team = team;
-  currentSmSlide = 0;
-  currentMdSlide = 3;
-  currentLgSlide = 6;
-
-  prevSmSlide() {
-    this.currentSmSlide = this.currentSmSlide === 0 ? this.team.length - 1 : this.currentSmSlide - 1;
-  }
-
-  nextSmSlide() {
-    this.currentSmSlide = this.currentSmSlide === this.team.length - 1 ? 0 : this.currentSmSlide + 1;
-  }
-
-  prevMdSlide() {
-    this.currentMdSlide = this.currentMdSlide === 0 ? this.team.length - 1 : this.currentMdSlide - 1;
-  }
-
-  nextMdSlide() {
-    this.currentMdSlide = this.currentMdSlide === this.team.length - 1 ? 0 : this.currentMdSlide + 1;
-  }
-
-  prevLgSlide() {
-    this.currentLgSlide = this.currentLgSlide === 0 ? this.team.length - 1 : this.currentLgSlide - 1;
-  }
-
-  nextLgSlide() {
-    this.currentLgSlide = this.currentLgSlide === this.team.length - 1 ? 0 : this.currentLgSlide + 1;
-  }
 }
