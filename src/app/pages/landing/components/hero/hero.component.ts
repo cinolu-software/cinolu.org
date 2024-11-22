3687ee4ae6279e97ee94897bcf6fba97be123bef
Wilfried Musanzi <musanziw.m@gmail.com>
2024-11-22 08:57:06 +0200

    fix: import router module for navigation

--- src/app/pages/landing/components/hero/hero.component.ts
+++ src/app/pages/landing/components/hero/hero.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { stakeholders } from './utils/data/stakeholders';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [MatIconModule, CommonModule],
+  imports: [MatIconModule, CommonModule, RouterModule],
   templateUrl: './hero.component.html'
 })
 export class HeroComponent {
