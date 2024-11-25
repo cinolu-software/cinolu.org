9464ee7e29c61dac94acc09aa0941673d2c4d255
Wilfried Musanzi <musanziw.m@gmail.com>
2024-11-25 14:50:03 +0200

    style: add loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoadingBarComponent } from 'app/shared/ui/loading-bar/loading-bar.component';
 import { TopbarComponent } from './shared/ui/topbar/topbar.component';
+import { LoaderComponent } from './shared/ui/loader/loader.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterOutlet, LoadingBarComponent, TopbarComponent]
+  imports: [RouterOutlet, LoadingBarComponent, TopbarComponent, LoaderComponent]
 })
 export class AppComponent {}
