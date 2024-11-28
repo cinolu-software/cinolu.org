513b98d3e4912eb445ea2a1c681a52ce9d722abb
Wilfried Musanzi <musanziw.m@gmail.com>
2024-11-28 09:31:50 +0200

    feat: add signal-based expansion functionality to program overview component

--- src/app/features/programs/feature/details/overview/overview.component.ts
+++ src/app/features/programs/feature/details/overview/overview.component.ts
@@ -1,17 +1,26 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { IProgram } from 'app/shared/utils/types/models.type';
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-program-overview',
-  imports: [MatIconModule, DatePipe, MatIconModule],
+  imports: [MatIconModule, DatePipe, MatIconModule, CommonModule],
   templateUrl: './overview.component.html'
 })
 export class ProgramOverviewComponent {
   program = input.required<IProgram>();
+  expanded = signal<string | null>(null);
 
   join(arr: unknown[]): string {
     return arr.map((a) => a['name']).join(', ');
+  }
+
+  expand(id: string): void {
+    if (this.expanded() === id) {
+      this.expanded.set(null);
+      return;
+    }
+    this.expanded.set(id);
   }
 }
