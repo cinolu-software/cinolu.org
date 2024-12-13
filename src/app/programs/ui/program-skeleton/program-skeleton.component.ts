6b62e82dfcb02139c53c5c4b239a15060976dcca
Wilfried Musanzi <musanziw.m@gmail.com>
2024-12-13 10:59:58 +0200

    refactor: remove unused MatIconModule

--- src/app/programs/ui/program-skeleton/program-skeleton.component.ts
+++ src/app/programs/ui/program-skeleton/program-skeleton.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-program-skeleton',
-  imports: [MatIconModule],
+  imports: [],
   templateUrl: './program-skeleton.component.html'
 })
 export class ProgramSkeletonComponent {}
