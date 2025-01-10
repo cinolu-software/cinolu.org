35e13276345c98a8f4fd5272b3ca52aab6b4721f
Wilfried Musanzi <musanziw.m@gmail.com>
2025-01-10 09:59:05 +0200

    refactor: rename program skeleton component to project skeleton component

--- src/app/projects/ui/program-skeleton/program-skeleton.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-program-skeleton',
-  imports: [],
-  templateUrl: './program-skeleton.component.html'
-})
-export class ProgramSkeletonComponent {}
