edaa29b3f7e45a84587b0a419a1ffbcc951b13f2
Wilfried Musanzi <musanziw.m@gmail.com>
2025-05-09 08:58:14 +0200

    feat: add topbar and footer components to home layout

--- src/app/about-us/feature/home/home.component.ts
+++ src/app/about-us/feature/home/home.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { AboutComponent } from '../about/about.component';
 import { AboutHeroComponent } from '../hero/about-hero.component';
 import { VisionComponent } from '../vision/vision.component';
+import { TopbarComponent } from '../../../shared/layout/ui/topbar/topbar.component';
+import { FooterComponent } from '../../../shared/layout/ui/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [AboutHeroComponent, AboutComponent, VisionComponent],
+  imports: [AboutHeroComponent, AboutComponent, VisionComponent, TopbarComponent, FooterComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {}
