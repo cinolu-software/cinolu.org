372c0256176ba4eeda75cfbdb98e0e704b17e011
Wilfried Musanzi <musanziw.m@gmail.com>
2025-05-09 08:57:48 +0200

    style: enhance about hero component layout and add fade-in animation

--- src/app/about-us/feature/hero/about-hero.component.ts
+++ src/app/about-us/feature/hero/about-hero.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgIcon } from '@ng-icons/core';
+import { fadeInStagger } from 'app/shared/animations/fade';
 
 @Component({
   selector: 'app-about-hero',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgIcon],
+  animations: [fadeInStagger],
   templateUrl: './about-hero.component.html'
 })
 export class AboutHeroComponent {}
