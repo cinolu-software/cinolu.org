fc7d4d9f094dcbef55f194ef5ee2d52737623a00
Wilfried Musanzi <musanziw.m@gmail.com>
2025-05-30 09:12:58 +0200

    style: update max-width classes to support 2xl breakpoint across various components

--- src/app/app.routes.server.ts
+++
@@ -1,8 +0,0 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
-
-export const serverRoutes: ServerRoute[] = [
-  // { path: 'posts', renderMode: RenderMode.Client },
-  { path: 'programs', renderMode: RenderMode.Client },
-  { path: 'events', renderMode: RenderMode.Client },
-  { path: '**', renderMode: RenderMode.Server },
-];
