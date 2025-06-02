ea1d566422962270cd82d29eb9dbee7d1e2fcf46
Wilfried Musanzi <musanziw.m@gmail.com>
2025-06-02 13:11:47 +0200

    feat: add navigation link to return to home in auth card component

--- src/app/auth/ui/auth-card/auth-card.component.ts
+++ src/app/auth/ui/auth-card/auth-card.component.ts
@@ -1,14 +1,16 @@
 import { Component, input } from '@angular/core';
 import { team } from './team';
 import { NgOptimizedImage } from '@angular/common';
+import { NgIcon } from '@ng-icons/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth-card',
-  imports: [NgOptimizedImage],
-  templateUrl: './auth-card.component.html'
+  imports: [NgOptimizedImage, NgIcon, RouterLink],
+  templateUrl: './auth-card.component.html',
 })
 export class AuthCardComponent {
-  team = team;
   title = input.required<string>();
   description = input.required<string>();
+  team = team;
 }
