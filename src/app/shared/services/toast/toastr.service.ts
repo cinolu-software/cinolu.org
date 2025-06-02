3cd3ebcd60855f8b71717d1169002141b6077b7d
Wilfried Musanzi <musanziw.m@gmail.com>
2025-06-02 13:30:11 +0200

    fix: update toastr notification position to bottom

--- src/app/shared/services/toast/toastr.service.ts
+++ src/app/shared/services/toast/toastr.service.ts
@@ -13,7 +13,7 @@ export class ToastrService {
       this.#ngZone.runOutsideAngular(() => {
         this.#notyf = new Notyf({
           duration: 4000,
-          position: { x: 'right', y: 'top' },
+          position: { x: 'right', y: 'bottom' },
         });
       });
     });
