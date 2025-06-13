b70092d16d08e598627e80757ce267c374dcaedb
Wilfried Musanzi <musanziw.m@gmail.com>
2025-06-13 09:08:02 +0200

    refactor: remove profile topbar component and update profile layout

--- src/app/shared/layout/feature/profile-layout/profile-layout.component.ts
+++ src/app/shared/layout/feature/profile-layout/profile-layout.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileMenuComponent } from '../../ui/profile-menu/profile-menu.component';
-import { ProfileTopbarComponent } from '../../ui/profile-topbar/profile-topbar.component';
 
 @Component({
   selector: 'app-profile-layout',
   templateUrl: './profile-layout.component.html',
-  imports: [RouterOutlet, ProfileMenuComponent, ProfileTopbarComponent],
+  imports: [RouterOutlet, ProfileMenuComponent],
 })
 export class ProfileLayoutComponent {}
