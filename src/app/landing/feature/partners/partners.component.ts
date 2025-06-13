11fdd75ae4a0bd25ff6775ba467b7695c8bdc3e2
Wilfried Musanzi <musanziw.m@gmail.com>
2025-06-13 16:28:15 +0200

    fix: update partners component layout and remove unused imports

--- src/app/landing/feature/partners/partners.component.ts
+++ src/app/landing/feature/partners/partners.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { partners } from '../../utils/data/partners';
-import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-partners',
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './partners.component.html',
 })
 export class PartnersComponent {
