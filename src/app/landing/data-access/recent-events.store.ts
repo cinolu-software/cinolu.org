5ea86656ba1a520b97326f826aad6e7620229303
Wilfried Musanzi <musanziw.m@gmail.com>
2025-06-20 16:48:49 +0200

    fix: remove error handling from recent events and projects store for simplification

--- src/app/landing/data-access/recent-events.store.ts
+++ src/app/landing/data-access/recent-events.store.ts
@@ -1,31 +1,29 @@
 import { signalStore, withState, withMethods, patchState, withHooks } from '@ngrx/signals';
 import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { catchError, exhaustMap, map, of, pipe, tap } from 'rxjs';
+import { catchError, exhaustMap, of, pipe, tap } from 'rxjs';
 import { IEvent } from '../../shared/utils/types/models.type';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 
 interface IRecentEventsStore {
   isLoading: boolean;
-  error: string | null;
   events: IEvent[];
 }
 
 export const RecentEventsStore = signalStore(
   withState<IRecentEventsStore>({
     isLoading: false,
-    error: null,
     events: []
   }),
   withMethods((store, http = inject(HttpClient)) => ({
     loadEvents: rxMethod<void>(
       pipe(
-        tap(() => patchState(store, { isLoading: true, error: null })),
+        tap(() => patchState(store, { isLoading: true })),
         exhaustMap(() => {
           return http.get<{ data: IEvent[] }>('events/find-recent').pipe(
-            map(({ data }) => patchState(store, { isLoading: false, events: data, error: null })),
-            catchError((error) => {
-              patchState(store, { isLoading: false, error: error['message'], events: [] });
+            tap(({ data }) => patchState(store, { isLoading: false, events: data })),
+            catchError(() => {
+              patchState(store, { isLoading: false, events: [] });
               return of([]);
             })
           );
