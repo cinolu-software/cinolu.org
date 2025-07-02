efd3297ecebb7c6debefc405b30a8ca3e60e6f8a
Wilfried Musanzi <musanziw.m@gmail.com>
2025-07-02 15:54:46 +0200

    refactor: streamline event loading logic and remove unused route handling

--- src/app/events/data-access/event.store.ts
+++ src/app/events/data-access/event.store.ts
@@ -1,36 +1,41 @@
-import { signalStore, withState, withMethods, patchState } from '@ngrx/signals';
+import { signalStore, withState, withMethods, patchState, withHooks, withProps } from '@ngrx/signals';
 import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { catchError, of, pipe, switchMap, tap } from 'rxjs';
 import { IEvent } from '../../shared/utils/types/models.type';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
 
 interface IEventStore {
   isLoading: boolean;
-  error: string | null;
   event: IEvent | null;
 }
 
 export const EventStore = signalStore(
-  withState<IEventStore>({
-    isLoading: false,
-    error: null,
-    event: null
-  }),
+  withState<IEventStore>({ isLoading: false, event: null }),
+  withProps(() => ({
+    _route: inject(ActivatedRoute)
+  })),
   withMethods((store, http = inject(HttpClient)) => ({
     loadEvent: rxMethod<string>(
       pipe(
-        tap(() => patchState(store, { isLoading: true, error: null })),
+        tap(() => patchState(store, { isLoading: true })),
         switchMap((slug) => {
           return http.get<{ data: IEvent }>(`events/slug/${slug}`).pipe(
-            tap(({ data }) => patchState(store, { isLoading: false, event: data, error: null })),
-            catchError((error) => {
-              patchState(store, { isLoading: false, error: error['message'] });
+            tap(({ data }) => patchState(store, { isLoading: false, event: data })),
+            catchError(() => {
+              patchState(store, { isLoading: false });
               return of(null);
             })
           );
         })
       )
     )
-  }))
+  })),
+  withHooks({
+    onInit: ({ _route, loadEvent }) => {
+      const slug = _route.snapshot.params['slug'];
+      loadEvent(slug);
+    }
+  })
 );
--- src/app/events/feature/details/event.component.ts
+++ src/app/events/feature/details/event.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule, Location, NgOptimizedImage } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, inject } from '@angular/core';
 import { EventSkeletonComponent } from '../../ui/event-skeleton/event-skeleton.component';
 import { ApiImgPipe } from '../../../shared/pipes/api-img.pipe';
 import { LucideAngularModule, ArrowLeft, FileText, NotepadText } from 'lucide-angular';
@@ -12,20 +11,14 @@ import { EventStore } from '../../data-access/event.store';
   imports: [CommonModule, EventSkeletonComponent, LucideAngularModule, NgOptimizedImage, ApiImgPipe],
   templateUrl: './event.component.html'
 })
-export class EventComponent implements OnInit {
-  #route = inject(ActivatedRoute);
+export class EventComponent {
   #location = inject(Location);
-  #slug = this.#route.snapshot.paramMap.get('slug') || '';
   icons = {
     moveLeft: ArrowLeft,
     fileText: FileText,
     notepadText: NotepadText
   };
   store = inject(EventStore);
-
-  ngOnInit(): void {
-    this.store.loadEvent(this.#slug);
-  }
 
   back(): void {
     this.#location.back();
