fd982582bcf3dbbb5fd7aea870cd7ddd5ed15544
Wilfried Musanzi <musanziw.m@gmail.com>
2025-07-02 11:27:31 +0200

    refactor: replace exhaustMap with switchMap in loadEnterprises method and clean up imports

--- src/app/profile/data-access/enterprises.store.ts
+++ src/app/profile/data-access/enterprises.store.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { signalStore, withState, withMethods, patchState, withHooks } from '@ngrx/signals';
+import { signalStore, withState, withMethods, patchState } from '@ngrx/signals';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
-import { pipe, tap, exhaustMap, catchError, of } from 'rxjs';
+import { pipe, tap, catchError, of, switchMap } from 'rxjs';
 import { IEnterprise } from '../../shared/utils/types/models.type';
+import { QueryParams } from '../utils/types/query-params.type';
+import { buildQueryParams } from '../../shared/utils/helpers/build-query-params.fn';
 
 interface IEnterprisesStore {
   isLoading: boolean;
@@ -13,24 +15,23 @@ interface IEnterprisesStore {
 export const EnterprisesStore = signalStore(
   withState<IEnterprisesStore>({ isLoading: false, enterprises: [[], 0] }),
   withMethods((store, http = inject(HttpClient)) => ({
-    loadEnterprises: rxMethod<void>(
+    loadEnterprises: rxMethod<QueryParams>(
       pipe(
         tap(() => patchState(store, { isLoading: true })),
-        exhaustMap(() => {
-          return http.get<{ data: [IEnterprise[], number] }>('enterprises/by-user').pipe(
-            tap(({ data }) => patchState(store, { isLoading: false, enterprises: data })),
-            catchError(() => {
-              patchState(store, { isLoading: false, enterprises: [[], 0] });
-              return of([]);
+        switchMap((queryParams) => {
+          return http
+            .get<{ data: [IEnterprise[], number] }>('enterprises/by-user', {
+              params: buildQueryParams(queryParams)
             })
-          );
+            .pipe(
+              tap(({ data }) => patchState(store, { isLoading: false, enterprises: data })),
+              catchError(() => {
+                patchState(store, { isLoading: false, enterprises: [[], 0] });
+                return of([]);
+              })
+            );
         })
       )
     )
-  })),
-  withHooks({
-    onInit: ({ loadEnterprises }) => {
-      loadEnterprises();
-    }
-  })
+  }))
 );
