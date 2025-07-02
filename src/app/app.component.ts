aa228ba247e466c83b0727e7ec67d633a1c24975
Wilfried Musanzi <musanziw.m@gmail.com>
2025-07-02 11:26:45 +0200

    feat: add loading bar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingBarComponent } from './shared/ui/loading-bar/loading-bar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterOutlet]
+  imports: [RouterOutlet, LoadingBarComponent]
 })
 export class AppComponent {}
