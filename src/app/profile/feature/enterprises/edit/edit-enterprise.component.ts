c65c8207dae35510a208763157b8ebaaa12fff7c
Wilfried Musanzi <musanziw.m@gmail.com>
2025-07-02 16:00:34 +0200

    refactor: create edit enterprise component and remove obsolete update component

---
+++ src/app/profile/feature/enterprises/edit/edit-enterprise.component.ts
@@ -0,0 +1,84 @@
+import { CommonModule, Location } from '@angular/common';
+import { Component, effect, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ArrowLeft, LucideAngularModule, ChevronsRight, ChevronsLeft, Check } from 'lucide-angular';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { Textarea } from 'primeng/textarea';
+import { SelectModule } from 'primeng/select';
+import { DatePickerModule } from 'primeng/datepicker';
+import { StepperModule } from 'primeng/stepper';
+import { SECTORS } from '../../../utils/data/sectors';
+import { STAGES } from '../../../utils/data/stage';
+import { Enterprisetore } from '../../../data-access/enterprise.store';
+import { UpdateEnterprisetore } from '../../../data-access/update-enterprise.store';
+
+@Component({
+  selector: 'app-edit-enterprise',
+  providers: [Enterprisetore, UpdateEnterprisetore],
+  imports: [
+    ReactiveFormsModule,
+    SelectModule,
+    ButtonModule,
+    DatePickerModule,
+    Textarea,
+    LucideAngularModule,
+    InputTextModule,
+    StepperModule,
+    CommonModule
+  ],
+  templateUrl: './edit-enterprise.component.html'
+})
+export class EditEnterpriseComponent {
+  #fb = inject(FormBuilder);
+  #location = inject(Location);
+  form: FormGroup;
+  icons = { back: ArrowLeft, next: ChevronsRight, previous: ChevronsLeft, check: Check };
+  sectors = SECTORS;
+  stages = STAGES;
+  enterpriseStore = inject(Enterprisetore);
+  updateEnterpriseStore = inject(UpdateEnterprisetore);
+
+  constructor() {
+    this.form = this.#fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      problem_solved: ['', Validators.required],
+      target_market: ['', Validators.required],
+      email: ['', Validators.email],
+      phone_number: [''],
+      website: [''],
+      linkedin_url: [''],
+      sector: [''],
+      founded_at: [''],
+      location: [''],
+      stage: ['']
+    });
+    effect(() => {
+      const enterprise = this.enterpriseStore.enterprise();
+      this.form.patchValue({
+        name: enterprise?.name,
+        description: enterprise?.description,
+        problem_solved: enterprise?.problem_solved,
+        target_market: enterprise?.target_market,
+        email: enterprise?.email,
+        phone_number: enterprise?.phone_number,
+        website: enterprise?.website,
+        linkedin_url: enterprise?.linkedin_url,
+        sector: enterprise?.sector,
+        founded_at: new Date(enterprise?.founded_at || ''),
+        location: enterprise?.location,
+        stage: enterprise?.stage
+      });
+    });
+  }
+
+  back(): void {
+    this.#location.back();
+  }
+
+  onAddEnterprise(): void {
+    if (!this.form.valid) return;
+    // this.store.addEnterprise(this.form.value);
+  }
+}
--- src/app/profile/feature/enterprises/update/update-enterprise.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-update-enterprise',
-  imports: [],
-  templateUrl: './update-enterprise.component.html'
-})
-export class UpdateEnterpriseComponent {}
