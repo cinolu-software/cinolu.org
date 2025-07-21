e744c022223df04476d75451efde3dad6d3ca09d
Wilfried Musanzi <musanziw.m@gmail.com>
2025-07-21 15:23:47 +0200

    feat(routes): add routes for programs, projects, and events in dashboard

--- src/app/dashboard/dashboard.routes.ts
+++ src/app/dashboard/dashboard.routes.ts
@@ -16,5 +16,20 @@ export const dashboardRoutes: Routes = [
     path: 'users',
     title: 'Les utilisateurs',
     loadComponent: () => import('./features/users/users.component').then((c) => c.DashboardUsersComponent)
+  },
+  {
+    path: 'programs',
+    title: 'Les programmes',
+    loadComponent: () => import('./features/programs/programs.component').then((c) => c.DashboardProgramsComponent)
+  },
+  {
+    path: 'projects',
+    title: 'Les projets',
+    loadComponent: () => import('./features/projects/projects.component').then((c) => c.DashboardProjectsComponent)
+  },
+  {
+    path: 'events',
+    title: 'Les événements',
+    loadComponent: () => import('./features/events/events.component').then((c) => c.DashboardEventsComponent)
   }
 ];
