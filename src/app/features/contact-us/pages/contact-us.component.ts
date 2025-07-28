83c302319e00fdb24ef8e11b32137502e2b4d6dc
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-07-28 14:59:01 +0200

    style: stylisation contact

--- src/app/features/contact-us/pages/contact-us.component.ts
+++ src/app/features/contact-us/pages/contact-us.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 import { CONTACT_ITEMS } from '../data/contact.data';
+import { FormsModule, NgForm } from '@angular/forms';
+import { countryCode } from '../data/countryItem.data';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, FormsModule],
+  standalone: true,
   templateUrl: './contact-us.component.html',
   styles: ``
 })
 export class ContactUsComponent {
+  countryItems = countryCode;
   contactItems = CONTACT_ITEMS;
+
+  formData = {
+    fullname: '',
+    email: '',
+    countryCode: '',
+    phone: '',
+    message: ''
+  };
+
+  onSubmit(contactForm: NgForm) {
+    console.log(contactForm.value);
+    contactForm.reset();
+  }
 }
