aa75c496140bdcc47c595b84365ad8638a1deb52
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-07-29 11:02:17 +0200

    perf: improve animation, title of heroComponent

--- src/app/features/landing/components/hero/hero.component.ts
+++ src/app/features/landing/components/hero/hero.component.ts
@@ -1,18 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, OnInit, OnDestroy } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { STATS } from '../../data/stats.data';
 import { LucideAngularModule, Lightbulb, MoveRight, HandCoins } from 'lucide-angular';
 import { RouterLink } from '@angular/router';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { titlesItems } from '../../data/title.data';
 
 @Component({
   selector: 'app-hero',
   imports: [LucideAngularModule, RouterLink],
-  templateUrl: './hero.component.html'
+  templateUrl: './hero.component.html',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(10px)' }),
+        animate('700ms ease-in-out', style({ opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [animate('700ms ease-in-out', style({ opacity: 0, transform: 'translateY(-10px)' }))])
+    ])
+  ],
+
+  standalone: true
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit, OnDestroy {
   stats = STATS;
   icons = {
     lightbulb: Lightbulb,
     arrowFlesh: MoveRight,
     donate: HandCoins
   };
+
+  rotatingTitles = titlesItems;
+
+  currentTitleIndex = 0;
+  currentTitle = this.rotatingTitles[0];
+
+  private intervalId: ReturnType<typeof setInterval> | undefined;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.intervalId = setInterval(() => {
+        this.currentTitleIndex = (this.currentTitleIndex + 1) % this.rotatingTitles.length;
+        this.currentTitle = this.rotatingTitles[this.currentTitleIndex];
+      }, 4000);
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
---
+++ src/app/features/landing/data/title.data.ts
@@ -0,0 +1,19 @@
+interface title {
+  first: string;
+  second: string;
+}
+
+export const titlesItems: title[] = [
+  {
+    first: 'Accélérons les talents,',
+    second: 'Connectons les opportunités.'
+  },
+  {
+    first: 'Construisons les villes',
+    second: 'Intelligentes d’Afrique.'
+  },
+  {
+    first: 'Un espace numérique',
+    second: 'pour bâtir demain.'
+  }
+];
