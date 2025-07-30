94480c1edb5e438bcfc716b70bd7c8d97607ba19
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-07-30 20:19:10 +0200

    feat: join us, inscription

--- src/app/features/join-us/data/member.items.ts
+++ src/app/features/join-us/data/member.items.ts
@@ -1,10 +1,10 @@
 import { DollarSign, GraduationCap, Heart, LucideIconData, UserCheck, UserCog } from 'lucide-angular';
 
 interface IMemberItem {
+  id: number;
   title: string;
   description: string;
   icon: LucideIconData;
-  path?: string;
 }
 
 interface IGender {
@@ -13,34 +13,40 @@ interface IGender {
 }
 export const MEMBER_ITEMS: IMemberItem[] = [
   {
+    id: 0,
+    title: 'Membre simple',
+    description: 'Rejoignez un réseau dynamique.',
+    icon: UserCog
+  },
+  {
+    id: 1,
     title: 'Je suis Entrepreneur',
     description: 'Rejoignez un réseau dynamique d’entrepreneurs et d’innovateurs.',
-    icon: UserCog,
-    path: '/sign-up/entrepreneur'
+    icon: UserCog
   },
   {
+    id: 2,
     title: 'Je suis Étudiant',
     description: 'Participez à des programmes de formation et d’accompagnement.',
-    icon: GraduationCap,
-    path: '/sign-up/student'
+    icon: GraduationCap
   },
   {
+    id: 3,
     title: 'Je suis Mentor / Coach',
     description: 'Contribuez à l’accompagnement des entrepreneurs et étudiants.',
-    icon: UserCheck,
-    path: '/sign-up/mentor'
+    icon: UserCheck
   },
   {
+    id: 4,
     title: 'Je suis volontaire/Stagiaire',
     description: 'Engagez-vous dans des projets innovants et enrichissants.',
-    icon: Heart,
-    path: '/sign-up/intern'
+    icon: Heart
   },
   {
+    id: 5,
     title: 'Je suis Investisseur / Partenaire',
     description: 'Investissez dans l’avenir des startups et de l’innovation.',
-    icon: DollarSign,
-    path: '/sign-up/investor'
+    icon: DollarSign
   }
 ];
 
