06f4f8891275e0f474381909510ae6ee2a7b197b
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-07-31 15:51:43 +0200

    refact: parteners style

--- src/app/features/landing/components/partners/partners.component.ts
+++ src/app/features/landing/components/partners/partners.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { PARTNERS } from '../../data/partners.data';
-import { NgOptimizedImage } from '@angular/common';
+import { PARTNERS, PARTNERS_CATEGORIES } from '../../data/partners.data';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-partners',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, CommonModule],
   templateUrl: './partners.component.html'
 })
 export class PartnersComponent {
   partners = PARTNERS;
+  categoryItems = PARTNERS_CATEGORIES;
+
+  statutItem = [
+    { id: 1, name: 'Étudiant·e' },
+    { id: 2, name: 'Entrepreneur·e' },
+    { id: 3, name: 'Volontaire' },
+    { id: 4, name: 'Chercheur·se' },
+    { id: 5, name: 'Autre' }
+  ];
+
+  date: Date | undefined;
+
+  selectedUserId = 0;
+
+  selectUserType(type: number) {
+    this.selectedUserId = type;
+  }
 }
