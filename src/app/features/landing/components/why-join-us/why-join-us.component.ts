b161743e332738823045415a1b5d8871fb7f7934
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-08-02 18:19:07 +0200

    u

--- src/app/features/landing/components/why-join-us/why-join-us.component.ts
+++ src/app/features/landing/components/why-join-us/why-join-us.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { STATS } from '../../data/stats.data';
-import { HandCoins, Lightbulb, LucideAngularModule, MoveUpRight } from 'lucide-angular';
+import { HandCoins, Lightbulb, LucideAngularModule, MoveUpRight, User } from 'lucide-angular';
 import { ButtonModule } from 'primeng/button';
 import { REASONS } from '../../data/reasons-join-us.data';
 
@@ -16,6 +16,7 @@ export class WhyJoinUsComponent {
   icons = {
     lightbulb: Lightbulb,
     moveUp: MoveUpRight,
-    donate: HandCoins
+    donate: HandCoins,
+    users: User
   };
 }
