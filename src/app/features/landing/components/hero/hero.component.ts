35f98b5b8b657519beca0e032f2cda8152f53bb3
Wilfried Musanzi <musanziw.m@gmail.com>
2025-08-04 12:37:34 +0200

    feat: update authentication forms and hero section content

--- src/app/features/landing/components/hero/hero.component.ts
+++ src/app/features/landing/components/hero/hero.component.ts
@@ -1,10 +1,8 @@
-import { Component, PLATFORM_ID, OnInit, OnDestroy, inject } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { Component } from '@angular/core';
 import { STATS } from '../../data/stats.data';
 import { Lightbulb, MoveRight, HandCoins, LucideAngularModule } from 'lucide-angular';
 import { RouterLink } from '@angular/router';
 import { trigger, transition, style, animate } from '@angular/animations';
-import { TITLE_ITEMS } from '../../data/title.data';
 
 @Component({
   selector: 'app-hero',
@@ -20,31 +18,7 @@ import { TITLE_ITEMS } from '../../data/title.data';
     ])
   ]
 })
-export class HeroComponent implements OnInit, OnDestroy {
-  #intervalId: ReturnType<typeof setInterval> | undefined;
-  #platformId = inject(PLATFORM_ID);
+export class HeroComponent {
   stats = STATS;
-  icons = {
-    lightbulb: Lightbulb,
-    arrowFlesh: MoveRight,
-    donate: HandCoins
-  };
-
-  rotatingTitles = TITLE_ITEMS;
-
-  currentTitleIndex = 0;
-  currentTitle = this.rotatingTitles[0];
-
-  ngOnInit(): void {
-    if (isPlatformBrowser(this.#platformId)) {
-      this.#intervalId = setInterval(() => {
-        this.currentTitleIndex = (this.currentTitleIndex + 1) % this.rotatingTitles.length;
-        this.currentTitle = this.rotatingTitles[this.currentTitleIndex];
-      }, 4000);
-    }
-  }
-
-  ngOnDestroy(): void {
-    if (this.#intervalId) clearInterval(this.#intervalId);
-  }
+  icons = { lightbulb: Lightbulb, arrowFlash: MoveRight, donate: HandCoins };
 }
