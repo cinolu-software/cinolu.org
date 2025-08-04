b3ae71c9d7458e30dd04b2dc71a9d2a210d0fa0a
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-08-04 11:45:23 +0200

    feat: form subscription

--- src/app/features/auth/pages/sign-up/sign-up.component.ts
+++ src/app/features/auth/pages/sign-up/sign-up.component.ts
@@ -1,13 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
 import { environment } from '../../../../../environments/environment';
 import { AuthCardComponent } from '../../components/auth-card/auth-card.component';
 import { SignUpStore } from '../../store/sign-up.store';
-import { FloatLabel, FloatLabelModule } from 'primeng/floatlabel';
+import { FloatLabelModule } from 'primeng/floatlabel';
 import { ArrowLeft, ArrowRight, LucideAngularModule } from 'lucide-angular';
 import { SelectModule } from 'primeng/select';
 import { StepperModule } from 'primeng/stepper';
@@ -19,17 +19,14 @@ import { GENDERS, MEMBER_ITEMS } from '../../../join-us/data/member.items';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  providers: [SignUpStore],
+  providers: [SignUpStore, DatePipe],
   imports: [
     InputTextModule,
     PasswordModule,
     ButtonModule,
     FormsModule,
     ReactiveFormsModule,
-    CommonModule,
     AuthCardComponent,
-    FloatLabel,
-
     LucideAngularModule,
     InputTextModule,
     FormsModule,
@@ -47,21 +44,23 @@ export class AuthSignUpComponent {
   #formBuilder: FormBuilder = inject(FormBuilder);
   form: FormGroup;
   store = inject(SignUpStore);
+  datePipe = inject(DatePipe);
 
   constructor() {
     this.form = this.#formBuilder.group({
-      fullname: ['', [Validators.minLength(5)]],
+      name: ['', [Validators.minLength(5)]],
       email: ['', [Validators.email, Validators.required]],
-      phone: ['', [Validators.required, Validators.pattern(/^\+?[1-9]\d{1,14}$/)]],
+      phone_number: ['', [Validators.required, Validators.pattern(/^\+?[1-9]\d{1,14}$/)]],
       gender: ['', [Validators.required]],
-      birthdate: ['', [Validators.required]],
-      statut: ['', [Validators.required]],
-      countryCode: ['', [Validators.required]],
-      adress: ['', [Validators.required, Validators.minLength(3)]],
-      reason: ['', [Validators.required]]
+      birth_date: ['', [Validators.required]],
+      profile: ['', [Validators.required]],
+      country: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      address: ['', [Validators.required, Validators.minLength(3)]],
+      reason: ['', [Validators.required]],
+      biography: ['', [Validators.required]]
     });
   }
-
   memberItems = MEMBER_ITEMS;
   icons = { next: ArrowRight, previous: ArrowLeft };
 
@@ -77,21 +76,17 @@ export class AuthSignUpComponent {
     { id: 5, name: 'Autre' }
   ];
 
-  date: Date | undefined;
-
-  selectedUserId = 0;
-
-  selectUserType(type: number) {
-    this.selectedUserId = type;
-  }
-
   onSignUp(): void {
-    if (this.form.invalid) {
-      return;
-    } else {
-      console.log(this.form.value);
-    }
-    // this.store.signUp(this.form.value);
+    if (this.form.invalid) return;
+
+    const rawForm = this.form.value;
+
+    const formattedForm = {
+      ...rawForm,
+      birth_date: this.datePipe.transform(rawForm.birth_date, 'dd/MM/yyyy')
+    };
+    console.log(formattedForm);
+    this.store.signUp(formattedForm);
   }
 
   signinWithGoogle(): void {
