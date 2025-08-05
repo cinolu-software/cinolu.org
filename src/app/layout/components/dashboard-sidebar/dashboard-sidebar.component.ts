bf119e3b4152a489354a4cd3eb0917ee6ac225ca
Wilfried Musanzi <musanziw.m@gmail.com>
2025-08-05 16:54:50 +0200

    refactor: update dashboard sidebar component for improved layout and role-based link management

--- src/app/layout/components/dashboard-sidebar/dashboard-sidebar.component.ts
+++ src/app/layout/components/dashboard-sidebar/dashboard-sidebar.component.ts
@@ -1,26 +1,31 @@
-import { Component, computed, inject, input, signal } from '@angular/core';
+import { Component, computed, effect, inject, input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
-import { LucideAngularModule, ChevronDown, Home } from 'lucide-angular';
-import { DASHBOARD_LINKS, ILink } from '../../data/links.data';
+import { LucideAngularModule, ChevronDown, House } from 'lucide-angular';
+import { USER_LINKS, ILink, ADMIN_LINKS } from '../../data/links.data';
 import { filter } from 'rxjs';
 import { AuthStore } from '../../../core/auth/auth.store';
 import { ButtonModule } from 'primeng/button';
-import { HasRoleDirective } from '../../../shared/directives/has-role.directive';
+import { RoleEnum } from '../../../core/auth/role.enum';
 
 @Component({
   selector: 'app-dashboard-sidebar',
-  imports: [CommonModule, ButtonModule, HasRoleDirective, RouterModule, LucideAngularModule],
+  imports: [CommonModule, ButtonModule, RouterModule, LucideAngularModule],
   templateUrl: './dashboard-sidebar.component.html'
 })
 export class DashboardSidebarComponent {
   #router = inject(Router);
   style = input<string>();
-  icons = { chevronDown: ChevronDown, home: Home };
-  links = signal<ILink[]>(DASHBOARD_LINKS);
+  icons = { chevronDown: ChevronDown, home: House };
   currentUrl = signal(this.#router.url);
   toggleTab = signal<string | null>(null);
   authStore = inject(AuthStore);
+  links = signal<ILink[]>([]);
+  dashboardLinks: Record<string, ILink[]> = {
+    [RoleEnum.User]: USER_LINKS,
+    [RoleEnum.Admin]: ADMIN_LINKS,
+    [RoleEnum.Coach]: ADMIN_LINKS
+  };
   activeTab = computed(() => {
     const url = this.currentUrl();
     return (
@@ -30,6 +35,15 @@ export class DashboardSidebarComponent {
   });
 
   constructor() {
+    const roles = (this.authStore.user()?.roles as unknown as string[]) || [];
+    effect(() => {
+      this.links.set(
+        roles.reduce((acc: ILink[], role: string) => {
+          if (this.dashboardLinks[role]) acc.push(...this.dashboardLinks[role]);
+          return acc;
+        }, [])
+      );
+    });
     this.#router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
       this.currentUrl.set(event.urlAfterRedirects);
     });
