cdc4f3b1b6173bb4acb47696b572579eb8180637
Wilfried Musanzi <musanziw.m@gmail.com>
2025-08-05 16:50:18 +0200

    refactor: update auth card component for improved navigation and layout

--- src/app/features/auth/components/auth-card/auth-card.component.ts
+++ src/app/features/auth/components/auth-card/auth-card.component.ts
@@ -1,21 +1,17 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { TEAM } from '../../data/team.data';
-import { Location, NgOptimizedImage } from '@angular/common';
+import { NgOptimizedImage } from '@angular/common';
 import { LucideAngularModule, ArrowLeft } from 'lucide-angular';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth-card',
-  imports: [NgOptimizedImage, LucideAngularModule],
+  imports: [NgOptimizedImage, RouterLink, LucideAngularModule],
   templateUrl: './auth-card.component.html'
 })
 export class AuthCardComponent {
   title = input.required<string>();
   description = input.required<string>();
   team = TEAM;
-  #location = inject(Location);
   icons = { arrowLeft: ArrowLeft };
-
-  onGoBack(): void {
-    this.#location.back();
-  }
 }
