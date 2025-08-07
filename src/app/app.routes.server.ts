9006fa968b5a1505fb841b261a7c46c0af23541f
Wilfried Musanzi <musanziw.m@gmail.com>
2025-08-07 11:40:01 +0200

    refactor: update server routes for clarity and consistency

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,31 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'programs/:slug',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'events/:slug',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'dashboard/ventures/list/update/:slug',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'dashboard/users/list/edit/:email',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'dashboard/projects/list/edit/:slug',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'dashboard/events/list/edit/:slug',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
