01c8c0f1d889b1cc7f9d33e3c2c8f3a701e0c80f
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-08-08 16:21:00 +0200

    style: stylization of our-team section

--- src/app/features/site/landing/components/partners/partners.component.ts
+++ src/app/features/site/landing/components/partners/partners.component.ts
@@ -3,24 +3,37 @@ import { PARTNERS, PARTNERS_CATEGORIES } from '../../data/partners.data';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
-import { Select } from 'primeng/select';
 import { FadeInOnScrollDirective } from '../../../../../shared/directives/animations-on-scroll.directive';
+import { RouterLink } from '@angular/router';
+import { UserPlus, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-partners',
-  imports: [NgOptimizedImage, CommonModule, FadeInOnScrollDirective, InputGroupModule, InputGroupAddonModule, Select],
-  templateUrl: './partners.component.html'
+  imports: [
+    NgOptimizedImage,
+    CommonModule,
+    FadeInOnScrollDirective,
+    InputGroupModule,
+    InputGroupAddonModule,
+    RouterLink,
+    LucideAngularModule,
+  ],
+  templateUrl: './partners.component.html',
 })
 export class PartnersComponent {
   partners = PARTNERS;
   categoryParteners = PARTNERS_CATEGORIES;
+
+  icons = {
+    userPlus: UserPlus,
+  };
 
   statutItem = [
     { id: 1, name: 'Étudiant·e' },
     { id: 2, name: 'Entrepreneur·e' },
     { id: 3, name: 'Volontaire' },
     { id: 4, name: 'Chercheur·se' },
-    { id: 5, name: 'Autre' }
+    { id: 5, name: 'Autre' },
   ];
 
   date: Date | undefined;
