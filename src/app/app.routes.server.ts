24b7ae0834bd5566ab73e7f6345dca8acb6a86eb
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-08-22 12:45:58 +0200

    feat: finalize first part blog section development on landing page

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -18,10 +18,6 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Client,
   },
   {
-    path: 'our-programs/:slug',
-    renderMode: RenderMode.Client,
-  },
-  {
     path: 'dashboard/ventures/update/:slug',
     renderMode: RenderMode.Client,
   },
