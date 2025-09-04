b2129fcf1230212cfba2d9b9f98319321bb31664
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-09-04 14:07:55 +0100

    feat(subprogram): add skeleton components for subprogram card and event card

---
+++ src/app/features/our-programs/pages/component/subprogram-event-card/subprogram-card.ts
@@ -0,0 +1,63 @@
+import { Component, input } from '@angular/core';
+import { IEvent } from '../../../../../shared/models/entities.models';
+import {
+  Calendar1,
+  ArrowLeft,
+  CalendarCheck,
+  CalendarMinus,
+  FileText,
+  FolderOpenDot,
+  LucideAngularModule,
+  MapPin,
+  MessageCircleMore,
+  MoveRight,
+  MoveUpRight,
+  NotepadText,
+  Tag,
+  ThumbsUp,
+  UserPlus,
+} from 'lucide-angular';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { ApiImgPipe } from '../../../../../shared/pipes/api-img.pipe';
+
+@Component({
+  selector: 'app-subprogram-card',
+  imports: [LucideAngularModule, CommonModule, NgOptimizedImage, ApiImgPipe],
+  templateUrl: './subprogram-card.html',
+  styles: ``,
+})
+export class SubprogramCard {
+  icons = {
+    moveLeft: ArrowLeft,
+    fileText: FileText,
+    notepadText: NotepadText,
+    userPlus: UserPlus,
+    tag: Tag,
+    comment: MessageCircleMore,
+    like: ThumbsUp,
+    calendar: Calendar1,
+    moveUp: MoveUpRight,
+    thumbsUp: ThumbsUp,
+    program: FolderOpenDot,
+    arrow: MoveRight,
+    place: MapPin,
+    startedAt: CalendarCheck,
+    endedAt: CalendarMinus,
+  };
+
+  event = input.required<IEvent>();
+
+  getStatut(project: IEvent): string {
+    const now = new Date();
+    const startedAt = new Date(project.started_at);
+    const endedAt = new Date(project.ended_at);
+
+    if (startedAt <= now && endedAt >= now) {
+      return 'En cours';
+    } else if (startedAt > now) {
+      return 'À venir';
+    } else {
+      return 'Terminé';
+    }
+  }
+}
