427c6e5b58157adbc162940b0b9c80add3132660
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-09-05 15:54:05 +0100

    feat(partners): ajouter le composant PartenersHero avec le modèle HTML et les animations

---
+++ src/app/features/our-parteners/component/hero/partenes-hero.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { LucideAngularModule, Info } from 'lucide-angular';
+
+@Component({
+  selector: 'app-parteners-hero',
+  imports: [RouterModule, LucideAngularModule],
+  templateUrl: './parteners-hero.html',
+})
+export class PartenersHero {
+  icons = {
+    info: Info,
+  };
+}
--- src/app/features/our-parteners/pages/our-parteners.ts
+++ src/app/features/our-parteners/pages/our-parteners.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { PARTNERS } from '../../landing/data/partners.data';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { PartenersHero } from '../component/hero/partenes-hero';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 
 @Component({
   selector: 'app-our-parteners',
-  imports: [NgOptimizedImage, CommonModule],
+  imports: [
+    NgOptimizedImage,
+    CommonModule,
+    PartenersHero,
+    AnimateOnScrollModule,
+  ],
   templateUrl: './our-parteners.html',
   styles: ``,
 })
