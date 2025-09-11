ce3da8575634d9386dba10f82ea943b7ce63c94f
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-09-11 10:41:59 +0100

    feat(partners): improve partners layout and animation, adjust image styles

--- src/app/features/our-parteners/pages/our-parteners.ts
+++ src/app/features/our-parteners/pages/our-parteners.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
 import { PARTNERS } from '../../landing/data/partners.data';
-import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { PartenersHero } from '../component/hero/partenes-hero';
 import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+import { Image } from 'primeng/image';
 
 @Component({
   selector: 'app-our-parteners',
-  imports: [
-    NgOptimizedImage,
-    CommonModule,
-    PartenersHero,
-    AnimateOnScrollModule,
-  ],
+  imports: [CommonModule, PartenersHero, AnimateOnScrollModule, Image],
   templateUrl: './our-parteners.html',
   styles: ``,
 })
