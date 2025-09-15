619680955758e1b151cf7a4c2da0bd598e8fd3de
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-09-15 15:45:13 +0100

    feat(gallery): add gallery structure with images and associated component

---
+++ src/app/features/galerry/data/gallery.data.ts
@@ -0,0 +1,67 @@
+interface galleryImage {
+  src: string;
+  alt: string;
+}
+
+export const GALLERY_IMAGES: galleryImage[] = [
+  {
+    src: 'images/gallery/1.jpg',
+    alt: 'Atelier de formation',
+  },
+  {
+    src: 'images/gallery/2.jpg',
+    alt: 'Atelier de formation',
+  },
+  {
+    src: 'images/gallery/3.jpg',
+    alt: 'Atelier de formation',
+  },
+  {
+    src: 'images/gallery/4.jpg',
+    alt: 'Atelier de formation',
+  },
+  {
+    src: 'images/gallery/5.jpg',
+    alt: 'Atelier de formation',
+  },
+  {
+    src: 'images/gallery/6.jpg',
+    alt: 'Atelier de formation',
+  },
+  {
+    src: 'images/gallery/7.jpg',
+    alt: 'Atelier de formation',
+  },
+  {
+    src: 'images/gallery/8.jpg',
+    alt: 'Atelier de formation',
+  },
+  {
+    src: 'images/gallery/9.jpg',
+    alt: 'Atelier de formation',
+  },
+  {
+    src: 'images/gallery/10.jpg',
+    alt: 'Atelier de formation',
+  },
+  {
+    src: 'images/gallery/11.jpg',
+    alt: 'Atelier de formation',
+  },
+  {
+    src: 'images/gallery/12.jpg',
+    alt: 'Atelier de formation',
+  },
+  {
+    src: 'images/gallery/13.jpg',
+    alt: 'Atelier de formation',
+  },
+  {
+    src: 'images/gallery/14.jpg',
+    alt: 'Atelier de formation',
+  },
+  {
+    src: 'images/gallery/15.jpg',
+    alt: 'Atelier de formation',
+  },
+];
