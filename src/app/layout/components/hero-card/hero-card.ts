0907acc3643161945a23823efbfef3a457d1a771
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-09-18 08:35:59 +0100

    style(hero-card): improve component styling

--- src/app/layout/components/hero-card/hero-card.ts
+++ src/app/layout/components/hero-card/hero-card.ts
@@ -1,3 +1,4 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { LucideIconData, LucideAngularModule } from 'lucide-angular';
@@ -6,6 +7,23 @@ import { LucideIconData, LucideAngularModule } from 'lucide-angular';
   selector: 'app-hero-card',
   imports: [LucideAngularModule, CommonModule],
   templateUrl: './hero-card.html',
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(10px)' }),
+        animate(
+          '1000ms ease-in-out',
+          style({ opacity: 1, transform: 'translateY(0)' }),
+        ),
+      ]),
+      transition(':leave', [
+        animate(
+          '1000ms ease-in-out',
+          style({ opacity: 0, transform: 'translateY(-10px)' }),
+        ),
+      ]),
+    ]),
+  ],
 })
 export class HeroCard {
   @Input({ required: true }) background!: string;
