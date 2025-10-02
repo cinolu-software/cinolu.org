4b08499c23ad9cda64c38e1c8f116dde3cce3049
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-10-02 09:00:21 +0100

    style(profil entrepreneur): stilyzation and integration icon social network

--- src/app/features/our-entrepreneurs/components/company-detail-card/company-detail-card.ts
+++ src/app/features/our-entrepreneurs/components/company-detail-card/company-detail-card.ts
@@ -6,7 +6,8 @@ import {
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { HeroCard } from '../../../../layout/components/hero-card/hero-card';
-import { LucideAngularModule, Users } from 'lucide-angular';
+import { LucideAngularModule, MoveRight, Users } from 'lucide-angular';
+import { SOCIAL_LINKS } from '../../../contact-us/data/contact.data';
 
 @Component({
   selector: 'app-company-detail-card',
@@ -16,11 +17,12 @@ import { LucideAngularModule, Users } from 'lucide-angular';
 export class CompanyDetailCard implements OnInit {
   entrepreneur?: IEntrepreneur;
   route = inject(ActivatedRoute);
+  socialLinks = SOCIAL_LINKS;
 
   icons = {
     users: Users,
+    moveRight: MoveRight,
   };
-
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
