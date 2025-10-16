41c561ac71a68d1cf16827274703a76a0533fa7b
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-10-16 13:45:08 +0100

    feat: implement product detail component with image gallery and dynamic data binding

--- src/app/features/our-entrepreneurs/components/product-detail/product-detail.ts
+++ src/app/features/our-entrepreneurs/components/product-detail/product-detail.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductDetailsStore } from '../../store/product.store';
+import { ActivatedRoute } from '@angular/router';
+import { carouselConfig } from '../../../landing/config/carousel.config';
+import { ApiImgPipe } from '../../../../shared/pipes/api-img.pipe';
+import { NgOptimizedImage } from '@angular/common';
+import { GalleriaModule } from 'primeng/galleria';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  providers: [ProductDetailsStore],
+  imports: [ApiImgPipe, NgOptimizedImage, GalleriaModule],
   templateUrl: './product-detail.html',
 })
-export class ProductDetail {
+export class ProductDetail implements OnInit {
+  store = inject(ProductDetailsStore);
+  route = inject(ActivatedRoute);
+  responsiveOptions = carouselConfig;
 
+  ngOnInit(): void {
+    const slug = this.route.snapshot.params['slug'];
+    this.store.loadProduct(slug);
+  }
 }
