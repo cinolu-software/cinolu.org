de012bd1feba930e74ef7ff3bd08cb4af8617ae3
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-10-16 12:55:01 +0100

    feat: add produit detail composant

---
+++ src/app/features/our-entrepreneurs/components/product-detail/product-detail.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail',
+  imports: [],
+  templateUrl: './product-detail.html',
+})
+export class ProductDetail {
+
+}
