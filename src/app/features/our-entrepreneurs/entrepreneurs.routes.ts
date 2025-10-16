3c9d860a3341c8eb7b33407183b9b4793ec00831
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-10-16 14:05:01 +0100

    feat: add product detail route

--- src/app/features/our-entrepreneurs/entrepreneurs.routes.ts
+++ src/app/features/our-entrepreneurs/entrepreneurs.routes.ts
@@ -16,4 +16,9 @@ export const entrepreneursRoutes = [
     loadComponent: () =>
       import('./components/venture-card-detail/venture-card-detail').then((c) => c.VentureCardDetail),
   },
+  {
+    path: 'product/slug/:slug',
+    title: 'Product - Details',
+    loadComponent: () => import('./components/product-detail/product-detail').then((c) => c.ProductDetail),
+  },
 ];
