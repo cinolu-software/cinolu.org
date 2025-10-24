536a58a7408effce9ebe76c8873a7ebd5a937ae3
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-10-24 15:25:37 +0100

    feat: add a featured card skeleton to improve display on loading

---
+++ src/app/features/highlights/component/highlight-card-skeleton/highlight-card-skeleton.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-highlight-card-skeleton',
+  imports: [],
+  templateUrl: './highlight-card-skeleton.html',
+  styles: ``
+})
+export class HighlightCardSkeleton {
+
+}
--- src/app/features/landing/components/highlights/highlights.ts
+++ src/app/features/landing/components/highlights/highlights.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HighlightCard } from '../../../highlights/component/highlight-card/highlight-card';
 import { HighlightsStore } from '../../../highlights/store/highlights.store';
+import { HighlightCardSkeleton } from "../../../highlights/component/highlight-card-skeleton/highlight-card-skeleton";
 
 @Component({
   selector: 'app-highlights',
   standalone: true,
-  imports: [CommonModule, HighlightCard],
+  imports: [CommonModule, HighlightCard, HighlightCardSkeleton],
   providers: [HighlightsStore],
   templateUrl: './highlights.html',
 })
