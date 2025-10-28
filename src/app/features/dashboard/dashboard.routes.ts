c94c67e812156aff7dac928e5859706456cec567
Wilfried Musanzi <musanziw.m@gmail.com>
2025-10-28 14:48:54 +0200

    feat: add edit venture component with form handling and gallery management

--- src/app/features/dashboard/dashboard.routes.ts
+++ src/app/features/dashboard/dashboard.routes.ts
@@ -4,46 +4,46 @@ export const dashboardRoutes: Routes = [
   {
     path: '',
     title: 'Dashboard',
-    loadComponent: () => import('./pages/dashboard/pages/dashboard').then((c) => c.Dashboard),
+    loadComponent: () => import('./pages/dashboard/pages/dashboard').then((c) => c.Dashboard)
   },
   {
     path: 'community',
-    loadChildren: () => import('./pages/outreach/outreach.routes').then((c) => c.outreachRoutes),
+    loadChildren: () => import('./pages/outreach/outreach.routes').then((c) => c.outreachRoutes)
   },
   {
     path: 'account',
-    loadChildren: () => import('./pages/account/account.routes').then((c) => c.accountRoutes),
+    loadChildren: () => import('./pages/account/account.routes').then((c) => c.accountRoutes)
   },
   {
     path: 'ventures',
-    loadChildren: () => import('./pages/ventures/ventures.routes').then((c) => c.venturesRoutes),
+    loadChildren: () => import('./pages/ventures/ventures.routes').then((c) => c.venturesRoutes)
   },
   {
     path: 'products',
-    loadChildren: () => import('./pages/products/products.routes').then((c) => c.productsRoutes),
+    loadChildren: () => import('./pages/products/products.routes').then((c) => c.productsRoutes)
   },
   {
     path: '',
-    loadChildren: () => import('./pages/users/users.routes').then((c) => c.usersRoutes),
+    loadChildren: () => import('./pages/users/users.routes').then((c) => c.usersRoutes)
   },
   {
     path: '',
-    loadChildren: () => import('./pages/programs/programs.routes').then((c) => c.programsRoutes),
+    loadChildren: () => import('./pages/programs/programs.routes').then((c) => c.programsRoutes)
   },
   {
     path: '',
-    loadChildren: () => import('./pages/projects/projects.routes').then((c) => c.projectsRoutes),
+    loadChildren: () => import('./pages/projects/projects.routes').then((c) => c.projectsRoutes)
   },
   {
     path: '',
-    loadChildren: () => import('./pages/events/events.routes').then((c) => c.eventsRoutes),
+    loadChildren: () => import('./pages/events/events.routes').then((c) => c.eventsRoutes)
   },
   {
     path: 'blog',
-    loadChildren: () => import('./pages/blog/blog.routes').then((c) => c.blogRoutes),
+    loadChildren: () => import('./pages/blog/blog.routes').then((c) => c.blogRoutes)
   },
   {
     path: 'entrepreneurs',
-    loadChildren: () => import('./pages/our-entrepreneurs/entrepreneurs.routes').then((c) => c.entrepreneursRoutes),
-  },
+    loadChildren: () => import('./pages/entrepreneurs/entrepreneurs.routes').then((c) => c.entrepreneursRoutes)
+  }
 ];
