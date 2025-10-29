70f4c91cae4649428d4cf697dd6160b4c45450b3
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-10-29 13:47:21 +0100

    feat(entrepreneur-user-card): update venture image handling and improve routing

--- src/app/features/entrepreneurs/components/entrepreneur-user-card/entrepreneur-user-card.ts
+++ src/app/features/entrepreneurs/components/entrepreneur-user-card/entrepreneur-user-card.ts
@@ -1,29 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Award, LucideAngularModule, MoveRight, Star } from 'lucide-angular';
 import { RouterLink } from '@angular/router';
-import { IUser } from '../../../../shared/models/entities.models';
+import { IVenture } from '../../../../shared/models/entities.models';
 import { ApiImgPipe } from '../../../../shared/pipes/api-img.pipe';
 import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-entrepreneur-user-card',
-  imports: [
-    CommonModule,
-    LucideAngularModule,
-    ApiImgPipe,
-    NgOptimizedImage,
-    RouterLink,
-    Button,
-  ],
+  imports: [CommonModule, LucideAngularModule, ApiImgPipe, NgOptimizedImage, RouterLink, Button],
   templateUrl: './entrepreneur-user-card.html',
+  standalone: true
 })
 export class EntrepreneurUserCard {
-  @Input() entrepreneur!: IUser;
+  entrepreneurs = input.required<IVenture>();
 
   icons = {
     award: Award,
     verified: Star,
-    moveRight: MoveRight,
+    moveRight: MoveRight
   };
 }
