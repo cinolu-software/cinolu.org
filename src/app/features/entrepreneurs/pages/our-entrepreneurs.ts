cd8092314601d4037a8a28e76a41d14777a2e259
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-10-29 13:50:09 +0100

    feat: refactor entrepreneur component to use ventures store for data handling

--- src/app/features/entrepreneurs/pages/our-entrepreneurs.ts
+++ src/app/features/entrepreneurs/pages/our-entrepreneurs.ts
@@ -2,29 +2,29 @@ import { Component, inject, OnInit } from '@angular/core';
 import { LucideAngularModule, Users } from 'lucide-angular';
 import { HeroCard } from '../../../layout/components/hero-card/hero-card';
 import { PaginatorModule, PaginatorState } from 'primeng/paginator';
-import { EntrepreneursStore } from '../store/entrepreneurs.store';
+import { VenturesStore } from '../store/ventures.store';
 import { EntrepreneurUserCard } from '../components/entrepreneur-user-card/entrepreneur-user-card';
 import { EntrepreneurCardSkeleton } from '../components/entrepreneur-card-skeleton/entrepreneur-card-skeleton';
 
 @Component({
   selector: 'app-our-entrepreneurs',
-  providers: [EntrepreneursStore],
+  providers: [VenturesStore],
   imports: [LucideAngularModule, HeroCard, PaginatorModule, EntrepreneurUserCard, EntrepreneurCardSkeleton],
-  templateUrl: './our-entrepreneurs.html',
+  templateUrl: './our-entrepreneurs.html'
 })
 export class OurEntrepreneurs implements OnInit {
   icons = { users: Users };
-  entrepreneurs = inject(EntrepreneursStore);
+  venturesStore = inject(VenturesStore);
 
   first = 0;
   rows = 8;
 
   ngOnInit() {
-    this.entrepreneurs.loadEntrepreneurs();
+    this.venturesStore.loadVentures();
   }
 
-  get pagedEntrepreneurs() {
-    const data = this.entrepreneurs.entrepreneurs();
+  get pagedVentures() {
+    const data = this.venturesStore.ventures();
     return data.slice(this.first, this.first + this.rows);
   }
 
