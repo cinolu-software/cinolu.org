dfe1653faa7fc75840af83848f9fa4b167d84b2c
Wilfried Musanzi <musanziw.m@gmail.com>
2025-10-31 11:30:04 +0200

    fix: update page title format in PageTitleStrategy

--- src/app/core/strategies/page-title.strategy.ts
+++ src/app/core/strategies/page-title.strategy.ts
@@ -15,7 +15,7 @@ export class PageTitleStrategy extends TitleStrategy {
   override updateTitle(routerState: RouterStateSnapshot) {
     const title = this.buildTitle(routerState);
     if (title !== undefined) {
-      this.#title.setTitle(`One Stop CINOLU - ${title}`);
+      this.#title.setTitle(`One Stop - ${title}`);
     }
   }
 }
