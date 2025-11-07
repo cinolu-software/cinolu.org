e846b47ddf9d6de9ffc34541d2dc1194dde76c71
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-11-07 11:11:24 +0100

    fix(our-team): refactor team member display for improved layout and accessibility

--- src/app/features/about-us/components/our-team/our-team.ts
+++ src/app/features/about-us/components/our-team/our-team.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { IMemberItem, TEAM_MEMBERS } from '../../data/our-team.data';
 import { CommonModule } from '@angular/common';
+import { SOCIAL_LINKS } from '@features/contact-us/data/contact.data';
 
 @Component({
   selector: 'app-our-team',
   imports: [CommonModule],
-  templateUrl: './our-team.html',
-  styles: ``,
+  templateUrl: './our-team.html'
 })
 export class OurTeam {
   our_team = TEAM_MEMBERS;
 
+  socialLinks = SOCIAL_LINKS;
   selectedMember: IMemberItem = TEAM_MEMBERS[0];
 
   selectMember(member: IMemberItem): void {
