9a2b97302beec9960b54fb3c2383d46db16a16f8
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-11-10 15:14:41 +0100

    feat(partners): refactor partners section for improved design and user experience feat(recent-programs): enhance recent programs layout and button styling fix(project-card): update project card styles for better presentation and usability

--- src/app/features/landing/components/recent-programs/recent-programs.ts
+++ src/app/features/landing/components/recent-programs/recent-programs.ts
@@ -4,13 +4,17 @@ import { RouterLink } from '@angular/router';
 import { ProgramsStore } from '../../store/programs.store';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { ApiImgPipe } from '../../../../shared/pipes';
+import { LucideAngularModule, MoveRight } from 'lucide-angular';
 
 @Component({
   selector: 'app-recent-programs',
   providers: [ProgramsStore],
-  imports: [ButtonModule, RouterLink, ApiImgPipe, CommonModule, NgOptimizedImage],
+  imports: [ButtonModule, RouterLink, ApiImgPipe, CommonModule, NgOptimizedImage, LucideAngularModule],
   templateUrl: './recent-programs.html'
 })
 export class RecentPrograms {
+  icons = {
+    MoveRight: MoveRight
+  };
   store = inject(ProgramsStore);
 }
