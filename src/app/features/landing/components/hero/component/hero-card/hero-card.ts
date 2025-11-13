b051d4c61b5be9d6289b0a96347f1f6d503d8deb
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-11-13 14:48:24 +0100

    feat(i18n): implement translation support for hero card component

--- src/app/features/landing/components/hero/component/hero-card/hero-card.ts
+++ src/app/features/landing/components/hero/component/hero-card/hero-card.ts
@@ -3,10 +3,11 @@ import { RouterLink } from '@angular/router';
 import { LucideAngularModule, MoveRight, HandCoins } from 'lucide-angular';
 import { IHeroSlide } from '../../../../data/hero-slides.data';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero-card',
-  imports: [LucideAngularModule, RouterLink, CommonModule],
+  imports: [LucideAngularModule, RouterLink, CommonModule, TranslateModule],
   templateUrl: './hero-card.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
