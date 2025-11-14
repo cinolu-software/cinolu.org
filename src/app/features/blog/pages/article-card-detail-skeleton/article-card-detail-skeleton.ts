c56a8c9af63212add42ce71f3b931b99085d39ea
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-11-14 11:29:34 +0100

    feat(skeleton): add ArticleCardDetailSkeleton component

---
+++ src/app/features/blog/pages/article-card-detail-skeleton/article-card-detail-skeleton.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-article-card-detail-skeleton',
+  imports: [CommonModule],
+  templateUrl: './article-card-detail-skeleton.html',
+  styles: ``
+})
+export class ArticleCardDetailSkeleton {}
