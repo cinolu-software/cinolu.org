0e6628490aa56ce0a72e332f72f1b5a1b759d7bb
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-11-14 09:24:07 +0100

    fix(landing): correct component import and usage for programs

--- src/app/features/landing/pages/landing.ts
+++ src/app/features/landing/pages/landing.ts
@@ -4,13 +4,13 @@ import { Partners } from '../components/partners/partners';
 import { RecentEvents } from '../components/recent-events/recent-events';
 import { RecentProjects } from '../components/recent-projects/recent-projects';
 import { WhyJoinUs } from '../components/why-join-us/why-join-us';
-import { RecentPrograms } from '../components/recent-programs/recent-programs';
+import { Programs } from '../components/programs/programs';
 import { Onestop } from '../components/onestop/onestop';
 import { Services } from '../components/services/services';
 
 @Component({
   selector: 'app-landing',
-  imports: [Hero, RecentProjects, RecentEvents, Partners, WhyJoinUs, RecentPrograms, Onestop, Services],
+  imports: [Hero, RecentProjects, RecentEvents, Partners, WhyJoinUs, Programs, Onestop, Services],
   templateUrl: './landing.html'
 })
 export class Landing {}
