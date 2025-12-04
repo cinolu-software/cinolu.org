420e82f03bd2c7747ed4d1789b4e037399790536
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-12-04 13:49:47 +0100

    feat(testimonials): refactor testimonial card layout and enhance accessibility

--- src/app/features/landing/components/testimonials-section/testimonial-card/testimonial-card.ts
+++ src/app/features/landing/components/testimonials-section/testimonial-card/testimonial-card.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
-import { Testimonial } from '../../data/testimonials.data';
-import { LucideAngularModule, Quote } from 'lucide-angular';
+import { Testimonial } from '@features/landing/data/testimonials.data';
+import { LucideAngularModule, Quote, Star } from 'lucide-angular';
 
 @Component({
   selector: 'app-testimonial-card',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, CommonModule],
   templateUrl: './testimonial-card.html'
 })
 export class TestimonialCard {
   testimonial = input.required<Testimonial>();
   quoteIcon = Quote;
+  starIcon = Star;
 }
