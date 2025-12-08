8d30d5cc208f027e44a7ebafa8cf2f6f4cad290b
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-12-08 14:48:29 +0100

    feat(faq): add FAQ page skeleton with HTML and TypeScript

---
+++ src/app/features/faq/components/faq-page-skeleton/faq-page-skeleton.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-page-skeleton',
+  standalone: true,
+  imports: [],
+  templateUrl: './faq-page-skeleton.html',
+  styles: ``
+})
+export class FaqPageSkeleton {}
