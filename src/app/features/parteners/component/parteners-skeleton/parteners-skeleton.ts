68d2d7fa022c9591686880b433cc7940a62ba17c
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-12-10 13:10:16 +0100

    feat(partners): add PartnersSkeleton component with loading template

---
+++ src/app/features/parteners/component/parteners-skeleton/parteners-skeleton.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parteners-skeleton',
+  imports: [],
+  templateUrl: './parteners-skeleton.html'
+})
+export class PartenersSkeleton {}
