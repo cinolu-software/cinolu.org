aacb9d3255d3f217f0c33d442aff0235d526ba69
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-12-10 08:45:32 +0100

    feat(environment): add brevoApiKey and production flag to environment configurations

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,7 @@ export const environment = {
   apiUrl: 'http://localhost:8000/',
   appUrl: 'http://localhost:4200/',
   onestopUrl: 'http://localhost:4000/',
-  analyticsId: 'G-GDJZH4CYPJ'
+  analyticsId: 'G-GDJZH4CYPJ',
+  brevoApiKey: 'J3A1HyKpCZg5R6Qn',
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,7 @@ export const environment = {
   apiUrl: 'https://api.cinolu.org/',
   appUrl: 'https://cinolu.org/',
   onestopUrl: 'https://onestop.cinolu.org/',
-  analyticsId: 'G-GDJZH4CYPJ'
+  analyticsId: 'G-GDJZH4CYPJ',
+  brevoApiKey: 'J3A1HyKpCZg5R6Qn',
+  production: false
 };
