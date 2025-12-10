4eb69e91433a8e5fa0f1c79d46817e3d8dbe817a
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2025-12-10 13:09:05 +0100

    refactor(faq): simplify import paths in FAQ components

--- src/app/features/faq/faq.routes.ts
+++ src/app/features/faq/faq.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const faqRoutes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/faq-page/faq-page').then((m) => m.FaqPage),
+    loadComponent: () => import('./pages/faq-page').then((m) => m.FaqPage),
     title: 'FAQ - Cinolu'
   }
 ];
