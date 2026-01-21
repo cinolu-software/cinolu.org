f28e5851399215ba2d43675ec26acf294891a329
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2026-01-21 09:00:20 +0100

    refactor(networks): enhance layout and optimize imports for better performance

--- src/app/features/landing/components/networks/networks.ts
+++ src/app/features/landing/components/networks/networks.ts
@@ -1,12 +1,12 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { LucideAngularModule, Network, Globe, Users } from 'lucide-angular';
 import { TranslateModule } from '@ngx-translate/core';
 import { NETWORKS } from '@features/landing/data/networks.data';
 
 @Component({
   selector: 'app-networks',
-  imports: [NgOptimizedImage, CommonModule, LucideAngularModule, TranslateModule],
+  imports: [CommonModule, LucideAngularModule, TranslateModule],
   templateUrl: './networks.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
