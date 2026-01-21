51818c846759ccced177f1b5313b187ddf7a1b27
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2026-01-21 15:14:39 +0100

    fix(api-img): update environment import path and clean up commented code

--- src/app/shared/pipes/api-img.pipe.ts
+++ src/app/shared/pipes/api-img.pipe.ts
@@ -1,34 +1,5 @@
-// import { Pipe, PipeTransform } from '@angular/core';
-// import { environment } from '../../../environments/environment';
-
-// @Pipe({
-//   name: 'apiIMG',
-//   pure: true
-// })
-// export class ApiImgPipe implements PipeTransform {
-//   transform(v: unknown, key: string): string {
-//     const apiUrl = environment.apiUrl;
-//     const value = v as Record<string, string>;
-//     const images: Record<string, string> = {
-//       program: value['logo'] ? `${apiUrl}uploads/programs/${value['logo']}` : '/images/no-image.jpg',
-//       gallery: `${apiUrl}uploads/galleries/${value['image']}`,
-//       subprogram: value['logo'] ? `${apiUrl}uploads/subprograms/${value['logo']}` : '/images/no-image.jpg',
-//       user: value['profile']
-//         ? `${apiUrl}uploads/profiles/${value['profile']}`
-//         : (value['google_image'] ?? '/images/avatar-default.webp'),
-//       project: value['cover'] ? `${apiUrl}uploads/projects/${value['cover']}` : '/images/no-image.jpg',
-//       venture: value['logo'] ? `${apiUrl}uploads/ventures/logos/${value['logo']}` : '/images/no-image.jpg',
-//       ventureCover: value['cover'] ? `${apiUrl}uploads/ventures/covers/${value['cover']}` : '/images/no-image.jpg',
-//       product: value['image'] ? `${apiUrl}uploads/products/images/${value['image']}` : '/images/no-image.jpg',
-//       article: value['image'] ? `${apiUrl}uploads/articles/${value['image']}` : '/images/no-image.jpg',
-//       event: value['cover'] ? `${apiUrl}uploads/events/${value['cover']}` : '/images/no-image.jpg'
-//     };
-//     return images[key];
-//   }
-// }
-
 import { Pipe, PipeTransform } from '@angular/core';
-import { environment } from '@environments/environment.development';
+import { environment } from '@environments/environment';
 
 @Pipe({
   name: 'apiIMG',
@@ -87,7 +58,6 @@ export class ApiImgPipe implements PipeTransform {
         return typeof value['cover'] === 'string' && value['cover']
           ? `${apiUrl}uploads/events/${value['cover']}`
           : '/images/no-image.jpg';
-
       default:
         return '/images/no-image.jpg';
     }
