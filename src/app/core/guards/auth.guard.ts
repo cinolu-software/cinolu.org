940f56c0b15148da27f68dc22e5e0c8eb51d6247
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2026-01-26 15:36:30 +0100

    feat(auth): add returnUrl parameter to sign-in navigation in auth guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -2,11 +2,15 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthStore } from '../auth/auth.store';
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = (route) => {
   const authStore = inject(AuthStore);
   const router = inject(Router);
 
   if (authStore.user()) return true;
-  router.navigate(['/sign-in']);
+
+  // Construire le returnUrl à partir de la route courante
+  const returnUrl = '/' + route.url.map((segment) => segment.path).join('/');
+
+  router.navigate(['/sign-in'], { queryParams: { returnUrl } });
   return false;
 };
