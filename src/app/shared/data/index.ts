86f8b6635c6ed45fbeead5b8962101185a815dfd
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2026-02-02 12:14:58 +0100

    refactor(shared): remove unnecessary comments from central export files

--- src/app/shared/data/index.ts
+++ src/app/shared/data/index.ts
@@ -1,6 +1,2 @@
-/**
- * Central export file for all shared data
- */
-
 export * from './country-item.data';
 export * from './genders.data';
--- src/app/shared/models/index.ts
+++ src/app/shared/models/index.ts
@@ -1,5 +1,1 @@
-/**
- * Central export file for all shared models
- */
-
 export * from './entities.models';
