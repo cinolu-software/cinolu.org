0408577f3dc7de2b35220dced8c3e30736a49be3
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2026-02-16 13:17:14 +0100

    refactor(parteners): remove unused routes and component files related to partners

--- src/app/features/parteners/component/parteners-skeleton/parteners-skeleton.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-parteners-skeleton',
-  imports: [],
-  templateUrl: './parteners-skeleton.html'
-})
-export class PartenersSkeleton {}
