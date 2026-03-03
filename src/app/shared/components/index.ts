0b7ab8b8b040e22e661eccfd472e8b735f084cb2
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2026-03-03 15:23:54 +0100

    refactor(components): remove unused exports from shared components index

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,5 +1,1 @@
 export * from './back-button/back-button';
-export * from './circular-progress/circular-progress';
-export * from './stat-card/stat-card';
-export * from './progress-bar/progress-bar';
-export * from './activity-list/activity-list';
