b7239e84650725c8062a84ae749731f3009ab4db
AckeemMbuebua <ackeemmbuebua100@gmail.com>
2026-03-05 10:26:32 +0100

    feat(entrepreneur-detail-card-skeleton): add skeleton component for loading state

---
+++ src/app/features/entrepreneurs/components/entrepreneur-detail-card-skeleton/entrepreneur-detail-card-skeleton.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entrepreneur-detail-card-skeleton',
+  standalone: true,
+  imports: [],
+  templateUrl: './entrepreneur-detail-card-skeleton.html'
+})
+export class EntrepreneurDetailCardSkeleton {}
